891fd17103712d51883a284982a584e5456f4f84
Aman Gupta <amangupta0874@gmail.com>
2026-04-14 14:24:09 +0530

    added mongodbatlas

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const dbgr = require('debug')("development:mongoose");
+//    mongodb+srv://amanecommerce:sdcTEg2hA3aO5b2a@cluster0.z8n4zk.mongodb.net/?appName=Cluster0
+
 
 mongoose
-    .connect(`${config.get("MONGODB_URI")}/ecommercestore`)
+    .connect(`${config.get("MONGODB_URI")}`)
     .then(function () {
         dbgr("Connnected");
     })
